0c44177671f4bf4ad3f0326a8bbc6d2ae506fc61
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-07 19:39:17 -0800

    learning about fetch requests and then displaying it in a list

--- App.js
+++ App.js
@@ -1,19 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, FlatList, ScrollView, ActivityIndicator } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
+
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      isLoading:true,
+    }
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  render(){ 
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    } else{
+      return (
+        <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item})=> <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id},index)=>id}
+        />
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
+ 
+      )
+    }
+  }
+}
+
+
