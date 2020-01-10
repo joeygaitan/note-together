a554cf0c8a5fb92e752d1b6eaa8b9043f02d540a
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-10 09:54:04 -0800

    updated the single note page

--- components/Note/Note.js
+++ components/Note/Note.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import { Button,Text,View } from 'react-native';
 
-const Note = () => {
+class Note extends Component{
+    constructor(){
+        super(props);
 
-    return (
-        <View>
-            <Text>Title</Text>
-            <Text>Body</Text>
-        </View>
-    );
+        this.state = {
+            note: {},
+            open: false
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    getNote = () => {
+        const id = this.props.match.params.id
+
+        return fetch(`https://polar-dawn-63323.herokuapp.com/blogs${id}`)
+        .then((response) => response.json())
+        .then((responseData)=>{
+            const note = responseData.data;
+            this.setState({note, open: false})
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+
+    delete = () => {
+        this.props.delete(this.props.match.params.id)
+        this.props.history.push('/')
+    }
+
+    render(){
+        return (
+            <View>
+                <Text>Title</Text>
+                <Text>Body</Text>
+            </View>
+        );
+    }
 }
 
 export default Note;
--- components/Notes/Notes.js
+++ components/Notes/Notes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, FlatList, ScrollView, ActivityIndicator,ListItem } from 'react-native';
+import { Link } from "react-router-native";
 
 const Notes = (props) => {
     return (
     <View style={{flex: 1, paddingTop:50, paddingLeft:20,paddingRight:20}}>
             <FlatList
             data={props.notes}
-            renderItem={({item}) => 
-            <Text>{item.desc}, {item.header}</Text>
+            renderItem={({item}) =>
+            <Link to={`${item.id}`}><Text>{item.desc}, {item.header}</Text></Link>
             }
-            keyExtractor={({id}, index) => id}
             />
     </View>
     );
